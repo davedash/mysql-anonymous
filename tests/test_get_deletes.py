97d4bdbc0cd4036fd644ab618b3a09aaf1d9d33d
Henrique Lopes <riquellopes@gmail.com>
2018-05-10 20:24:55 -0300

    writing a new test for sql delete

--- tests/test_get_deletes.py
+++ tests/test_get_deletes.py
@@ -5,3 +5,17 @@ def test_should_get_the_empty_list():
     truncates = get_deletes({})
 
     assert truncates == []
+
+
+def test_should_get_the_list_of_delete_itens():
+    deleted = get_deletes({
+        "database": {
+            "tables": {
+                "user": {
+                    "delete": {"id": 1}
+                }
+            }
+        }
+    })
+
+    assert deleted == ['DELETE FROM `user` WHERE `id` = "1"']
