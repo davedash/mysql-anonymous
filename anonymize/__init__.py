adcf6da9fb5d87d74fd942160acdf6e194c0a9b9
Henrique <everaldo.lopes@concrete.com.br>
2018-05-11 08:01:23 -0300

    Refactoring
    * creating a setup for project.
    * renaming samples files.
    * simple example of option parse.

--- anonymize/__init__.py
+++ anonymize/__init__.py
@@ -3,9 +3,19 @@ from __future__ import absolute_import
 from optparse import OptionParser
 
 
+class Anonymize(object):
+    pass
+
+
 def main():
     parser = OptionParser()
     parser.add_option('-y', '--yaml')
+    parser.add_option('-s', '--sample', default=1)
+    (options, args) = parser.parse_args()
+
+    anonymize = Anonymize()
+    anonymize.run()
+
 
 if __name__ == "__main__":
     main()
---
+++ setup.py
@@ -0,0 +1,42 @@
+"""
+    Contributors can benefit from having real data when they are developing. This script can do a few things (see anonymize.yml):
+
+    Truncate any tables (logs, and other cruft which may have sensitive data)
+
+    Nullify fields (emails, passwords, etc)
+
+    Fill in random/arbitrary data:
+
+    Random integers
+    Random IP addresses
+    Email addresses
+    Usernames
+    Delete rows based on simple rules: e.g. DELETE FROM mytable WHERE private = "Yes":
+
+    database: tables: mytable: delete: private: Yes
+"""
+
+from setuptools import setup
+
+setup_params = {
+    "entry_points": {
+        "console_scripts": [
+            "anonymize=anonymize:main"
+        ]
+    }
+}
+
+
+setup(
+    author="Dave Dash",
+    author_email="dd+github@davedash.com, contato@henriquelopes.com.br",
+    version='0.2',
+    name="Mysql Anonymous",
+    url="https://github.com/davedash/mysql-anonymous",
+    packages=["anonymize"],
+    platforms=['python >= 2.7'],
+    description=__doc__,
+    long_description=__doc__,
+    py_modules=["anonymize"],
+    **setup_params
+)
