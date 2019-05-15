a3ec5fc3b71bb4d0cd9b9b1efd240f94d12aeaf9
William Valentim <williamvalentim@gmail.com>
2019-05-15 14:30:36 -0300

    added mysql functions to randomize names

--- anonymize.py
+++ anonymize.py
@@ -119,6 +119,22 @@ def anonymize(config):
          print("USE `%s`;" % database['name'])
 
     print("SET FOREIGN_KEY_CHECKS=0;")
+    print("DROP FUNCTION IF EXISTS generate_fname;")
+    print("      DELIMITER $$")
+    print("      CREATE FUNCTION generate_fname () RETURNS VARCHAR(255)")
+    print("BEGIN")
+    print('RETURN ELT(FLOOR(1 + (RAND() * (100-1))), "James","Mary","John","Patricia","Robert","Linda","Michael","Barbara","William","Elizabeth","David","Jennifer","Richard","Maria","Charles","Susan","Joseph","Margaret","Thomas","Dorothy","Christopher","Lisa","Daniel","Nancy","Paul","Karen","Mark","Betty","Donald","Helen","George","Sandra","Kenneth","Donna","Steven","Carol","Edward","Ruth","Brian","Sharon","Ronald","Michelle","Anthony","Laura","Kevin","Sarah","Jason","Kimberly","Matthew","Deborah","Gary","Jessica","Timothy","Shirley","Jose","Cynthia","Larry","Angela","Jeffrey","Melissa","Frank","Brenda","Scott","Amy","Eric","Anna","Stephen","Rebecca","Andrew","Virginia","Raymond","Kathleen","Gregory","Pamela","Joshua","Martha","Jerry","Debra","Dennis","Amanda","Walter","Stephanie","Patrick","Carolyn","Peter","Christine","Harold","Marie","Douglas","Janet","Henry","Catherine","Carl","Frances","Arthur","Ann","Ryan","Joyce","Roger","Diane");')
+    print("END$$")
+
+    print("DELIMITER ;")
+
+    print("DROP FUNCTION IF EXISTS generate_lname;")
+    print("DELIMITER $$")
+    print("CREATE FUNCTION generate_lname () RETURNS VARCHAR(255)")
+    print("BEGIN")
+    print('RETURN ELT(FLOOR(1 + (RAND() * (100-1))), "Smith","Johnson","Williams","Jones","Brown","Davis","Miller","Wilson","Moore","Taylor","Anderson","Thomas","Jackson","White","Harris","Martin","Thompson","Garcia","Martinez","Robinson","Clark","Rodriguez","Lewis","Lee","Walker","Hall","Allen","Young","Hernandez","King","Wright","Lopez","Hill","Scott","Green","Adams","Baker","Gonzalez","Nelson","Carter","Mitchell","Perez","Roberts","Turner","Phillips","Campbell","Parker","Evans","Edwards","Collins","Stewart","Sanchez","Morris","Rogers","Reed","Cook","Morgan","Bell","Murphy","Bailey","Rivera","Cooper","Richardson","Cox","Howard","Ward","Torres","Peterson","Gray","Ramirez","James","Watson","Brooks","Kelly","Sanders","Price","Bennett","Wood","Barnes","Ross","Henderson","Coleman","Jenkins","Perry","Powell","Long","Patterson","Hughes","Flores","Washington","Butler","Simmons","Foster","Gonzales","Bryant","Alexander","Russell","Griffin","Diaz","Hayes");')
+    print("END$$")
+    print("DELIMITER ;")
 
     sql = []
     sql.extend(get_truncates(config))
